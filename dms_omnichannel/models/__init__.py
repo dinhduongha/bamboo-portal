cbf54b2abdbd71518dbd719c2ef4dd6a415ac64d
Dinh Duong Ha <dinhduongha@gmail.com>
2026-08-30 00:24:31 +0700

    Delete dms_*

--- dms_omnichannel/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import dms_channel_account
-from . import dms_channel_identity
-from . import dms_channel_order
-from . import dms_channel_status
-from . import dms_channel_inbox
-from . import dms_channel_migration
